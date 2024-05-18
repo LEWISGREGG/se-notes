a5d356a27d4d983ba204f7e21febb5d0d8b22b61
SERGIO <40707060+LEWISGREGG@users.noreply.github.com>
2024-05-18 18:00:01 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'cdn.sanity.io',
+            port: '',
+            
+        },
+    {
+            protocol: 'https',
+            hostname: 'i.imgur.com',
+            port: '', 
+    },
+    {
+            protocol: 'https',
+            hostname: 'www.w3.org',
+            port: '', 
+    },
+    {
+            protocol: 'https',
+            hostname: 'www.svgrepo.com',
+            port: '', 
+    }]
+    }
+};
 
 export default nextConfig;
+
+
+
