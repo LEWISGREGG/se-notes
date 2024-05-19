f1ac739de80be07c85d127ce648704c213a141d3
SERGIO <40707060+LEWISGREGG@users.noreply.github.com>
2024-05-19 17:27:25 -0400

    Add files via upload

--- structure_blog/sanity.config.ts
+++ structure_blog/sanity.config.ts
@@ -1,16 +1,14 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'se project',
-
   projectId: 'gvs747qo',
   dataset: 'production',
-
-  plugins: [structureTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
